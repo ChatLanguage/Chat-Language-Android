30b69dd0023ae7e29421f6f03e1b3dc273e1e243
ViniciusDeep <viniciuscorreia11@gmail.com>
2019-01-07 23:26:16 -0300

    Added new activit

---
+++ app/src/main/java/br/com/chatlaguage/vinicius/chatlanguage/ContactsActivity.java
@@ -0,0 +1,13 @@
+package br.com.chatlaguage.vinicius.chatlanguage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContactsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_contacts);
+    }
+}

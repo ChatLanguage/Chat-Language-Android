a69441bd873bc4dbe155904cd11f28ac3d595f37
ViniciusDeep <viniciuscorreia11@gmail.com>
2019-01-08 09:15:36 -0300

    Implemented fetch users

--- app/src/main/java/br/com/chatlaguage/vinicius/chatlanguage/ContactsActivity.java
+++ app/src/main/java/br/com/chatlaguage/vinicius/chatlanguage/ContactsActivity.java
@@ -1,7 +1,15 @@
 package br.com.chatlaguage.vinicius.chatlanguage;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class ContactsActivity extends AppCompatActivity {
 
@@ -9,5 +17,18 @@ public class ContactsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_contacts);
+        fetchUsers();
     }
+
+    private void fetchUsers() {
+        FirebaseFirestore.getInstance().collection("/users")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                        Log.e("Teste", e.getMessage(), e);
+                    }
+                });
+
+    }
+
 }
